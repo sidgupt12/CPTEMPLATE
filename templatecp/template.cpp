aebbe0df4c29b8daa7d7fe64a7d25fcb16edde2d
Siddhant Gupta <144828350+sidgupt12@users.noreply.github.com>
2024-04-26 09:30:51 +0530

    Add files via upload

---
+++ templatecp/template.cpp
@@ -0,0 +1,43 @@
+/*CHURANA MAT MERA TEMPLATE HAI*/
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+ 
+/*types*/
+#define ll long long
+
+/*TO HARD TO TYPE*/
+#define endl "\n"
+#define pb push_back
+#define eb emplace_back
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; //find_by_order, order_of_key
+ 
+/*functions*/
+#define f(s,e) for(int i=s;i<e;i++)
+#define rf(s,e) for(int i=e-1;i>=s;i--)
+#define print(x) cout << x << "\n";
+ 
+ 
+void solve(){
+    
+}
+ 
+ 
+int main(){
+//CP TEMPLATE - SIDDHANT
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+ 
+    //solve();
+ 
+    int t;cin>>t;
+ 
+    while(t--){
+        solve();
+    }
+ 
+return 0;
+}
